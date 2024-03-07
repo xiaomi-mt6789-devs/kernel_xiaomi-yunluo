6796b0ddec66f5ce5bda823e9e28bc87d5cee03f
bengris32 <bengris32@protonmail.ch>
2024-03-07 21:01:18 +0000

    drivers: power_throttling: Avoid wakeup spam during suspend
    * Looking at wakelock stats, "dlpt_notify_ws wakelock" has an absolutely
      absurd amount of wakeups compared to other wakesources.
    
    * This is due to the fact that there is a thread that is constantly
      running dlpt_notify_handler(). This function checks if the device is
      suspended or not and skips everything if the device is in suspend.
    
    * There is one glaring issue however, no matter if the device is in suspend
      or not, we will always hold the dlpt_notify_ws wakelock, and then immediately
      release it again after if the device is in suspend? What's the point? We've already
      triggered the device to wake up!
    
    * In order to fix this, make sure we skip aqcuiring the wakelock if we are already
      suspended by moving the __pm_stay_awake() call to after the dlpt.suspend_flag check.
    
    Change-Id: Ie7a0f956594e9956428c3943aeecceeec0f0b3d9
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/misc/mediatek/power_throttling/mtk_dynamic_loading_throttling.c
+++ drivers/misc/mediatek/power_throttling/mtk_dynamic_loading_throttling.c
@@ -433,10 +433,11 @@ static int dlpt_notify_handler(void *unused)
 
 		wait_event_interruptible(dlpt.notify_waiter,
 					 (dlpt.notify_flag == true));
-		__pm_stay_awake(dlpt.notify_ws);
 		mutex_lock(&dlpt.notify_lock);
 		if (dlpt.suspend_flag)
 			goto bypass;
+
+		__pm_stay_awake(dlpt.notify_ws);
 
 		cur_ui_soc = dlpt_get_uisoc();
 
@@ -468,9 +469,9 @@ static int dlpt_notify_handler(void *unused)
 			dlpt_set_shutdown_condition();
 			pr_info("[DLPT] notify battery SOC=0 to power off.\n");
 		}
+		__pm_relax(dlpt.notify_ws);
 bypass:
 		mutex_unlock(&dlpt.notify_lock);
-		__pm_relax(dlpt.notify_ws);
 
 		mod_timer(&dlpt.notify_timer, jiffies + dlpt_notify_interval);
 
